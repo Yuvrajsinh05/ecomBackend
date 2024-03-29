830d58f2312a93b62f353bd07460f5f63457e851
Yuvrajsinh05 <yuvrajsinh.jadav.21M@iitram.ac.in>
2024-03-30 04:19:18 +0530

    changes

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-const PORT2 = PORT + 1  || 8671;
+const PORT2 = PORT + 1;
 server2.listen(PORT2, () => {
   console.log(`Server 2 is running on port ${PORT2}`);
 });
